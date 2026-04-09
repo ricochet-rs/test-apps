c9d4a488fac22f9c3b37fbf23eba10e68a4da070
Josiah Parry <josiah.parry@gmail.com>
2026-04-09 14:10:24 -0700

    add py shiny test

---
+++ python/shiny-py-penguins/app.py
@@ -0,0 +1,20 @@
+
+from palmerpenguins import load_penguins
+from plotnine import aes, geom_histogram, ggplot, theme_minimal
+from shiny.express import input, render, ui
+
+dat = load_penguins()
+species = dat["species"].unique().tolist()
+
+ui.input_radio_buttons("species", "Species", species, inline=True)
+
+
+@render.plot(height=300)
+def plot():
+    sel = dat[dat["species"] == input.species()]
+    return (
+        ggplot(aes(x="bill_length_mm"))
+        + geom_histogram(dat, fill="#C2C2C4", binwidth=1)
+        + geom_histogram(sel, fill="#447099", binwidth=1)
+        + theme_minimal()
+    )
---
+++ python/shiny-py-penguins/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from shiny-py-penguins!")
+
+
+if __name__ == "__main__":
+    main()
