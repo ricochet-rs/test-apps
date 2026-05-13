9f0d7f634d581956057735c036d529633a9bda18
Josiah Parry <josiah.parry@gmail.com>
2026-05-13 07:41:12 -0700

    add diff entrypoint test

---
+++ r/diff-entrypoints/a.R
@@ -0,0 +1,44 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  # Application title
+  titlePanel("Entrypoint A"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  # Print message whenever slider changes
+  observeEvent(input$bins, {
+    message("Slider moved! New bin count: ", input$bins)
+  })
+
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    # draw the histogram with the specified number of bins
+    hist(
+      x,
+      breaks = bins,
+      col = "darkgray",
+      border = "white",
+      xlab = "Waiting time to next eruption (in mins)",
+      main = "Histogram of waiting times"
+    )
+  })
+}
+
+shiny::shinyApp(ui, server)
